a26b2042e288ac6a4ed2a92d4678a6d8bb38aa80
Gaurav Kumar <gk989kumar@gmail.com>
2024-03-22 11:32:26 +0530

    reset

---
+++ app/components/Login.tsx
@@ -0,0 +1,18 @@
+
+const Login = () => {
+  return (
+    <div>
+        <div className="flex items-center justify-center">
+            <p className="">Login</p>
+        <form>
+       <label htmlFor="fname">User Iddasfdsa:</label>
+       <input type="text" id="fname" name="fname"></input>
+     </form>
+
+        </div>
+  
+    </div>
+  )
+}
+
+export default Login
